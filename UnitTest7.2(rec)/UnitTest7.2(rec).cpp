d7a4cbe0b9caef98f514d7918762f6952b212aeb
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-11-22 20:43:00 +0200

    Initial commit

---
+++ UnitTest7.2(rec)/UnitTest7.2(rec).cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab7.2(rec).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest72rec
+{
+	TEST_CLASS(UnitTest72rec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+
+			int rowCount = 3;
+			int colCount = 1;
+			int** P = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				P[i] = new int[colCount];
+			P[0][0] = 1;
+			P[1][0] = 1;
+			P[2][0] = 4;
+			t = colMax(P, rowCount, 0, P[0][0], 0);
+
+			Assert::AreEqual(t, 4);
+
+
+		}
+	};
+}
